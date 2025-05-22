6ebcb2ce912ff78efe5302d75edd6a55bd35ddba
wangc <wangcheng.public@gmail.com>
2025-05-22 22:33:51 +0800

    复用同一个线程，接收传入的命令，但是当不存在提交的command的时候，会空跑消耗cpu资源

--- src/main/java/com/cheng/MyThreadPool.java
+++ src/main/java/com/cheng/MyThreadPool.java
@@ -5,7 +5,16 @@ import java.util.List;
 
 public class MyThreadPool {
 
-    List<Thread> threadList=new ArrayList<>();
+    List<Runnable> commandList = new ArrayList<>();
+
+    Thread thread = new Thread(() -> {
+        while (true) {
+            if (!commandList.isEmpty()) {
+                Runnable command = commandList.remove(0);
+                command.run();
+            }
+        }
+    });
 
     // 1.线程什么时候创建
     // 2.线程的runnable是什么?是我们提交的command吗?
@@ -13,8 +22,6 @@ public class MyThreadPool {
         // 这种方式存在的问题
         // 1.频繁创建线程消耗资源
         // 2.创建的线程没有被管理起来
-        Thread thread = new Thread(command);
-        threadList.add(thread);
-        thread.start();
+        commandList.add(command);
     }
 }
