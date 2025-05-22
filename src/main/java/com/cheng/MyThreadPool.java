a5403fb61c395dcd12f84c8471ddcc18db70d040
wangc <wangcheng.public@gmail.com>
2025-05-22 22:29:26 +0800

    创建的线程放到集合中管理，无法复用线程

--- src/main/java/com/cheng/MyThreadPool.java
+++ src/main/java/com/cheng/MyThreadPool.java
@@ -1,6 +1,11 @@
 package com.cheng;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyThreadPool {
+
+    List<Thread> threadList=new ArrayList<>();
 
     // 1.线程什么时候创建
     // 2.线程的runnable是什么?是我们提交的command吗?
@@ -8,6 +13,8 @@ public class MyThreadPool {
         // 这种方式存在的问题
         // 1.频繁创建线程消耗资源
         // 2.创建的线程没有被管理起来
-        new Thread(command).start();
+        Thread thread = new Thread(command);
+        threadList.add(thread);
+        thread.start();
     }
 }
