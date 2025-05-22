b390399922f6b58c02ac586b67e90ee76f079250
wangc <wangcheng.public@gmail.com>
2025-05-22 22:48:59 +0800

    使用阻塞队列

--- src/main/java/com/cheng/Main.java
+++ src/main/java/com/cheng/Main.java
@@ -3,8 +3,16 @@ package com.cheng;
 public class Main {
     public static void main(String[] args) {
         MyThreadPool myThreadPool = new MyThreadPool();
-        myThreadPool.execute(()->{
-            System.out.println("Hello from thread pool!");
-        });
+        for (int i = 0; i < 5; i++) {
+            myThreadPool.execute(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName());
+            });
+        }
+        System.out.println("主线程没有被阻塞");
     }
 }
--- src/main/java/com/cheng/MyThreadPool.java
+++ src/main/java/com/cheng/MyThreadPool.java
@@ -2,19 +2,29 @@ package com.cheng;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class MyThreadPool {
 
-    List<Runnable> commandList = new ArrayList<>();
+    BlockingQueue<Runnable> commandList = new ArrayBlockingQueue<>(1024);
 
     Thread thread = new Thread(() -> {
         while (true) {
-            if (!commandList.isEmpty()) {
-                Runnable command = commandList.remove(0);
+            try {
+                Runnable command = commandList.take();
                 command.run();
+                //     等待阻塞队列被填充元素的过程中，如果thread被中断了，不会继续等待，而去处理异常
+                //     几乎所有需要线程等待的函数，都有这个异常
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
-    });
+    },"唯一线程");
+
+    {
+        thread.start();
+    }
 
     // 1.线程什么时候创建
     // 2.线程的runnable是什么?是我们提交的command吗?
@@ -22,6 +32,8 @@ public class MyThreadPool {
         // 这种方式存在的问题
         // 1.频繁创建线程消耗资源
         // 2.创建的线程没有被管理起来
-        commandList.add(command);
+
+        //  offer会通过返回值判断是否成功向队列中添加元素，而不是抛异常
+        boolean offer = commandList.offer(command);
     }
 }
